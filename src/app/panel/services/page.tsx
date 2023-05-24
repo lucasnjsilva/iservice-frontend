fba573cf53a3a848b96f227cb35b1d3430795a0f
lucasnjsilva <lucasnathanj@gmail.com>
2023-05-24 10:24:53 -0300

    criada página de listagem de serviços

---
+++ src/app/panel/services/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import Layout from "@/app/layouts/authenticated";
+import Table from "@/components/Table";
+import { useRouter, usePathname } from "next/navigation";
+
+function Services() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const table = {
+    head: ["Nome", "Categoria"],
+    body: [
+      {
+        id: "a1",
+        name: "Mecânico automotivo",
+        category: "Mecânico",
+      },
+      {
+        id: "a2",
+        name: "Pintura automotiva",
+        category: "Pintor",
+      },
+      {
+        id: "a3",
+        name: "Eletricista automotivo",
+        category: "Eletricista",
+      },
+    ],
+  };
+
+  const handleEdit = (id: string) => router.push(`${pathname}/edit/${id}`);
+
+  const handleDelete = (id: string) => {
+    return console.log(id);
+  };
+
+  return (
+    <Layout title="Serviços" admin={true}>
+      <Table
+        table={table}
+        actions={{
+          view: false,
+          edit: true,
+          delete: true,
+        }}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+      />
+    </Layout>
+  );
+}
+
+export default Services;
