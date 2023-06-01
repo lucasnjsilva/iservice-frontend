d42e0ba03b52c739a84bd7eeb93a0f44d31fa51e
lucasnjsilva <lucasnathanj@gmail.com>
2023-06-01 14:37:04 -0300

    pagina de signup de customer conectada com api

--- src/app/signup/customer/page.tsx
+++ src/app/signup/customer/page.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import classes from "./style";
 import useStyle from "@/utils/cssHandler";
 import Layout from "../../layouts/unauthenticated/index";
 import { useRouter } from "next/navigation";
 import isAuthenticated from "@/services/isAuthenticated";
 
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+
 function CustomerSignUp() {
   const useClasses = useStyle(classes);
-
   const navigate = useRouter();
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     if (isAuthenticated()) {
@@ -16,19 +20,66 @@ function CustomerSignUp() {
     }
   }, [navigate]);
 
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formValues: Record<string, string> = {};
+
+    formData.forEach((value, key) => {
+      formValues[key] = value as string;
+    });
+
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formValues),
+    };
+
+    const request = await fetch(`${API_HOST}/customers`, options);
+
+    const { error: requestError, result } = await request.json();
+
+    if (requestError) {
+      if (requestError.message) {
+        setError(requestError.message);
+      } else {
+        setError(
+          "Ocorreu um erro ao tentar criar sua conta, por favor tente novamente."
+        );
+      }
+    }
+
+    if (result) {
+      alert("Cadastrado com sucesso.");
+      navigate.push("/signin/customer");
+    }
+  };
+
   return (
     <Layout>
       <div className={useClasses.container}>
         <div className={useClasses.wrapper}>
           <h2 className={useClasses.title}>Criar conta</h2>
 
-          <form className={useClasses.form}>
+          <form className={useClasses.form} onSubmit={handleFormSubmit}>
             <div className={useClasses.formGroup}>
+              {error ? <p className="text-sm text-red-500">{error}</p> : null}
               <div>
                 <label htmlFor="name" className={useClasses.label}>
                   Nome
                 </label>
                 <input name="name" type="text" className={useClasses.input} />
+              </div>
+
+              <div>
+                <label htmlFor="cpf" className={useClasses.label}>
+                  CPF
+                </label>
+                <input name="cpf" type="text" className={useClasses.input} />
               </div>
 
               <div>
@@ -56,7 +107,9 @@ function CustomerSignUp() {
                 <input name="phone" type="text" className={useClasses.input} />
               </div>
 
-              <button className={useClasses.button}>Cadastrar</button>
+              <button type="submit" className={useClasses.button}>
+                Cadastrar
+              </button>
             </div>
           </form>
         </div>
--- src/app/signup/provider/page.tsx
+++ src/app/signup/provider/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect } from "react";
 import classes from "./style";
 import useStyle from "@/utils/cssHandler";
