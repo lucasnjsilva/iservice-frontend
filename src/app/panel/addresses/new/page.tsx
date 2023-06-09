b6d85e8aa29e17d210d304581a2fcf0229522487
lucasnjsilva <lucasnathanj@gmail.com>
2023-06-09 16:06:53 -0300

    pagina de criar endereço conectada com api

--- src/app/panel/addresses/new/page.tsx
+++ src/app/panel/addresses/new/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import classes from "./style";
 import useStyle from "@/utils/cssHandler";
@@ -5,11 +7,27 @@ import Layout from "@/app/layouts/authenticated";
 import { useRouter } from "next/navigation";
 import { isCustomer } from "@/services/checkRole";
 import isAuthenticated from "@/services/isAuthenticated";
+import { requestHeader } from "@/services/api";
+import useSWR from "swr";
+
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+
+const ufFetcher = (url: string) => fetch(url).then((res) => res.json());
+const citiesFetcher = (url: string) => fetch(url).then((res) => res.json());
 
 function NewAddress() {
   const navigate = useRouter();
   const useClasses = useStyle(classes);
   const [isLoading, setIsLoading] = useState(true);
+
+  const [uf, setUf] = useState<string>("");
+  const [city, setCity] = useState<string>("");
+
+  const ufURL = "https://brasilapi.com.br/api/ibge/uf/v1";
+  const useUFFetcher = useSWR(ufURL, ufFetcher);
+
+  const citiesURL = `https://brasilapi.com.br/api/ibge/municipios/v1/${uf}`;
+  const useCitiesFetcher = useSWR(citiesURL, uf ? citiesFetcher : null);
 
   useEffect(() => {
     if (!isAuthenticated() || !isCustomer()) {
@@ -23,15 +41,78 @@ function NewAddress() {
     return <div>Carregando...</div>;
   }
 
+  const renderUFComponent = () => {
+    if (useUFFetcher.error) return null;
+    if (useUFFetcher.isLoading) return null;
+    if (!useUFFetcher.data) return null;
+
+    return useUFFetcher.data.map((item: any) => (
+      <option key={item.id} value={item.sigla}>
+        {item.nome}
+      </option>
+    ));
+  };
+
+  const renderCityComponent = () => {
+    if (uf) {
+      if (useCitiesFetcher.error) return null;
+      if (useCitiesFetcher.isLoading) return null;
+      if (!useCitiesFetcher.data) return null;
+
+      return useCitiesFetcher.data.map((item: any) => (
+        <option key={item.codigo_ibge} value={item.nome}>
+          {item.nome}
+        </option>
+      ));
+    }
+  };
+
+  const handleSave = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.currentTarget);
+    const formValues: Record<string, string> = {};
+
+    formData.forEach((value, key) => {
+      formValues[key] = value as string;
+    });
+
+    formValues.uf = uf;
+    formValues.city = city;
+
+    const request = await fetch(`${API_HOST}/customer_addresses`, {
+      method: "POST",
+      headers: requestHeader,
+      body: JSON.stringify(formValues),
+    });
+
+    const { error: requestError } = await request.json();
+
+    if (requestError) {
+      window.location.reload();
+
+      return alert(
+        "Ocorreu um erro ao tentar atualizar seus dados, por favor, tente novamente."
+      );
+    }
+
+    navigate.push("/panel/addresses");
+  };
+
   return (
     <Layout title="Endereços" admin={false}>
-      <form className={useClasses.form}>
+      <form className={useClasses.form} onSubmit={handleSave}>
         <div className={useClasses.formGroup}>
           <div>
             <label htmlFor="address" className={useClasses.label}>
               Endereço
             </label>
-            <input name="address" type="text" className={useClasses.input} />
+            <input
+              name="address"
+              type="text"
+              className={useClasses.input}
+              required
+            />
           </div>
 
           <div>
@@ -49,6 +130,7 @@ function NewAddress() {
               name="neighborhood"
               type="text"
               className={useClasses.input}
+              required
             />
           </div>
 
@@ -67,27 +149,53 @@ function NewAddress() {
           </div>
 
           <div>
-            <label htmlFor="city" className={useClasses.label}>
-              Cidade
+            <label htmlFor="uf" className={useClasses.label}>
+              Estado*
             </label>
-            <input name="city" type="text" className={useClasses.input} />
+            <select
+              required
+              name="uf"
+              value={uf}
+              className={useClasses.inputSelect}
+              onChange={(e) => setUf(e.target.value)}
+            >
+              <option value="">Selecione um...</option>
+              {renderUFComponent()}
+            </select>
           </div>
 
           <div>
-            <label htmlFor="uf" className={useClasses.label}>
-              Estado
+            <label htmlFor="city" className={useClasses.label}>
+              Cidade*
             </label>
-            <input name="uf" type="text" className={useClasses.input} />
+            <input
+              required
+              name="city"
+              list="cities"
+              placeholder="Cidades"
+              className={useClasses.input}
+              disabled={!uf}
+              value={uf && city ? city : ""}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <datalist id="cities">{renderCityComponent()}</datalist>
           </div>
 
           <div>
             <label htmlFor="cep" className={useClasses.label}>
               CEP
             </label>
-            <input name="cep" type="text" className={useClasses.input} />
+            <input
+              name="cep"
+              type="text"
+              className={useClasses.input}
+              required
+            />
           </div>
 
-          <button className={useClasses.button}>Salvar</button>
+          <button type="submit" className={useClasses.button}>
+            Salvar
+          </button>
         </div>
       </form>
     </Layout>
